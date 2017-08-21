b945bf1904eb39bd3c6edb495aa4586819ac26d8
Ori Rawlings <orirawlings@gmail.com>
2017-08-21 09:34:19 -0500

    _examples: Add code for status example

---
+++ _examples/status/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"gopkg.in/src-d/go-git.v4"
+	. "gopkg.in/src-d/go-git.v4/_examples"
+)
+
+// Show status of files in the working directory.
+func main() {
+	CheckArgs("<path>")
+	path := os.Args[1]
+
+	// We instance a new repository targeting the given path (the .git folder)
+	r, err := git.PlainOpen(path)
+	CheckIfError(err)
+
+	// Get the working directory for the repository
+	w, err := r.Worktree()
+	CheckIfError(err)
+
+	// Length of the HEAD history
+	Info("git status")
+
+	// Get the status of the WorkTree
+	s, err := w.Status()
+	CheckIfError(err)
+
+	fmt.Println(s)
+}
